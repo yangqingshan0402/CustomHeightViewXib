9a2ff88992486fdaffd2716fc2af13dc33249dbe
lenkeng <lenkeng@lll.local>
2018-04-25 16:33:54 +0800

    Initial Commit

---
+++ intrinstic/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  intrinstic
+//
+//  Created by lenkeng on 25/04/2018.
+//  Copyright © 2018 lenkeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ intrinstic/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  intrinstic
+//
+//  Created by lenkeng on 25/04/2018.
+//  Copyright © 2018 lenkeng. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
